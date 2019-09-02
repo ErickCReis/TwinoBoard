3f803799e11f37637ff27b25bf3eeed937b4dd06
Erick Reis <erickcorreareis@gmail.com>
2019-09-01 23:01:09 -0300

    Corrigindo comentário

--- Exemplo-Display/main.c
+++ Exemplo-Display/main.c
@@ -8,7 +8,7 @@ int main(void)
 
 	display_init();             // Inicializando display
 	display_time(2000);         // Configurando tempo do display
-	display_enable_rand();      // Abilitando cores sequenciais
+	display_enable_rand();      // Habilitando cores sequenciais
 	LPM0;                       // Entrando em Low Power Mode 0
 
 	return 0;
