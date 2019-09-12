37d8ba137112612010aa71caf28159665dedf311
Erick Reis <erickcorreareis@gmail.com>
2019-09-12 14:59:03 -0300

    Ajustando comentários

--- Exemplo-Temperatura/TI_USCI_I2C_master.h
+++ Exemplo-Temperatura/TI_USCI_I2C_master.h
@@ -14,9 +14,6 @@ void TI_USCI_I2C_receive(unsigned int byteCount, unsigned char *field);
 void TI_USCI_I2C_transmit(unsigned int byteCount, unsigned char *field, int tempRead);
 
 void wait_for_i2c_bus();
-char is_i2c_busy();
-
-//void fix_i2c();
 
 void read_register(unsigned char, unsigned char, unsigned char, char);
 extern unsigned char read_reg[2];
